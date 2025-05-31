457da583e04a90bcd499782c5fcb5b9b2acc4c02
Ramesh11011 <programming.ramesh@gmail.com>
2025-05-31 22:05:16 +0530

    completed Day 10 of the challenge

---
+++ Day 10/DeleteTheMiddleNode.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (!head || !head->next)
+            return nullptr;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // slow is pointing to the middle node
+        prev->next = slow->next;
+        delete slow;
+
+        return head;
+    }
+};
+
+// Utility to create a linked list from a vector
+ListNode* createList(const vector<int>& arr) {
+    if (arr.empty()) return nullptr;
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* current = head;
+    for (size_t i = 1; i < arr.size(); ++i) {
+        current->next = new ListNode(arr[i]);
+        current = current->next;
+    }
+    return head;
+}
+
+// Utility to print a linked list
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Solution sol;
+
+    vector<int> input = {1, 3, 4, 7, 1, 2, 6}; // Example 1
+    ListNode* head = createList(input);
+
+    cout << "Original list: ";
+    printList(head);
+
+    head = sol.deleteMiddle(head);
+
+    cout << "After deleting middle: ";
+    printList(head);
+
+    return 0;
+}

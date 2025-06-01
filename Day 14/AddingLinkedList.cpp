9bc4dc5d7d673fa192f5b0e66c5ccfc7979f4b63
Ramesh11011 <programming.ramesh@gmail.com>
2025-06-01 11:21:22 +0530

    Completed with Day 14 , adding numbers in linked list

---
+++ Day 14/AddingLinkedList.cpp
@@ -0,0 +1,39 @@
+// Adding Numbers using linked list
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // Dummy node to build the result list
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int sum = carry;
+
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+        }
+
+        return dummy->next;
+    }
+};
